5ac1f8414e93e0aba53075b1f3b14fa969e3a180
Alexandra <s.valua66@gmail.com>
2020-12-26 12:55:10 +0300

    change photos and text

--- src/components/Portfolio.svelte
+++ src/components/Portfolio.svelte
@@ -3,11 +3,11 @@
 
     let imgs = [
         { src: "https://i.ibb.co/YtJ2y9G/1.jpg", alt: "" },
-        { src: "https://i.ibb.co/VQWYRg1/2.jpg", alt: "" },
+        { src: "http://ibb.co.com/images/hbU7FiNv3EQ63dc24d8d6ae39de.jpg", alt: "" },
+        { src: "http://ibb.co.com/images/1HT9k84EvBAf601084360e55fe8.jpg", alt: "" },
+        { src: "http://ibb.co.com/images/cZbY6Y5YOTQcb4b637a06d1c85f.jpg", alt: "" },
         { src: "https://i.ibb.co/tLN0VZ1/3.jpg", alt: "" },
-        { src: "https://i.ibb.co/jbZDhJM/4.jpg", alt: "" },
-        { src: "https://i.ibb.co/rQFjjr2/5.jpg", alt: "" },
-        { src: "https://i.ibb.co/VLZtrNR/6.jpg", alt: "" },
+        { src: "http://ibb.co.com/images/rxQfoytWo5g50b4f8b5c051730a.jpg", alt: "" },
         { src: "https://i.ibb.co/Bw7knSx/7.jpg", alt: "" },
         { src: "https://i.ibb.co/cF4wp42/8.jpg", alt: "" },
         { src: "https://i.ibb.co/KKzWvpk/9.jpg", alt: "" },
